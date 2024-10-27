479cd4bd6ba874151e6e65bf74b9f74a68aeefda
Pooja Agrawal <sutti@Poojas-MacBook-Air.local>
2024-10-27 18:51:54 +0530

    added file till now

---
+++ ArrayInsertion.js
@@ -0,0 +1,23 @@
+//Insert a elemeny in a array at a specific position
+
+
+let arr = [20,80,90,50,67,2,65] 
+
+// function insertElement(){
+// let el = document.getElementById("inputByUser").value
+// let position = document.getElementById("inputPosition").value
+// for(i=arr.length-1;i>=0;i--){
+//     if(i>=position){
+//         arr[i+1] = arr[i]
+//         if(i== position){
+//             arr[i] = el
+//         }
+//     }  
+// }
+// console.warn(arr)
+// }
+
+
+//above functionality can be achieved using splice 
+// arr.splice(4,0,5000);
+// console.warn(arr)
---
+++ ArrayTraversal.js
@@ -0,0 +1,25 @@
+// const heading = React.createElement("h1",{id:"heading"},"Hello world");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading)
+
+//Array Traversal 
+//Given a list of array and print each element inside a array
+
+let arr = [9,2,1,3,78,67]
+// for(i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+//     document.write(`array ${i} element is ${arr[i]} <br>`)
+// }
+
+//Get the specific value of a array using input box and clicking the button 
+// function getElement(){
+//     let el = document.getElementById("inputByUser").value
+//     if(el < arr.length && typeof parseInt(el) === "number"){
+//         alert(`Array element at ${el} is ${arr[el]}`)
+//     }
+//     else{
+//         alert('Please enter a valid input value')
+//     }
+// }
+
+
